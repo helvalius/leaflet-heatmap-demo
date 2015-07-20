80cb840f4948a86d5092ae1f54897646b5b011db
Jan Nonnen <jan.nonnen@gmx.net>
2015-07-20 23:02:28 +0200

    add bower dependencies to leaflet and esri

--- client/app/app.js
+++ client/app/app.js
@@ -8,34 +8,13 @@ var data = [
     { tour: 19, lat: 52.1536925, lon: 11.6395728}
 ];
 
-//here comes the leaflet standard for the basemap:
+var map = L.map('map').setView([ 40.706, -73.926], 14);
 
-var map = L.map('map').setView([0, 0], 2);
-var OpenStreetMap_BlackAndWhite = L.tileLayer('http://{s}.www.toolserver.org/tiles/bw-mapnik/{z}/{x}/{y}.png', {
-attribution: '&copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, example by <a href="http://www.digital-geography.com">digital-geography.com</a>'
+L.esri.basemapLayer('Streets').addTo(map);
+
+L.esri.heatmapFeatureLayer({
+  url: 'https://services.arcgis.com/rOo16HdIMeOBI4Mb/ArcGIS/rest/services/Graffiti_Reports/FeatureServer/0',
+  radius: 12
 }).addTo(map);
-
-
-
-
-// now the function that is called after the file was loaded
-
-function displayData(Text){
-
- var data_array= [0,0,0]; //create it before filling
- for (var i = 0; i < data.length; i++) {
-   data_array[i] = [data[i].lat, data[i].lon,0]; // if values are marked as string in the object else:
- }
-
- var heat = L.heatLayer(data_array,{
-          radius: 15,
-          blur: 20,
-          maxZoom: 6,
-      }).addTo(map);
-}
-
-
-
-displayData(data);
 
 })();
--- gulpFile.js
+++ gulpFile.js
@@ -1,6 +1,7 @@
 (function(){
   var gulp  = require('gulp');
   var $     = require('gulp-load-plugins')({lazy:false});
+  var wiredep = require('wiredep').stream;
 
 $.livereload();
 $.livereload.listen();
@@ -13,30 +14,26 @@ var paths = {
   styles: './client/app/**/*.css'
 }
 
-gulp.task('default', $.sequence('inject', 'server', 'watch'));
+gulp.task('default', $.sequence('bower', 'server', 'watch'));
 gulp.task('server', startServer);
 gulp.task('watch', startWatch);
-gulp.task('inject', startInject)
+
 
 function startServer(){
   require('./server');
-
 }
+
 function startWatch(){
   gulp.watch('./client/app/**/*.css', $.livereload.changed);
   gulp.watch('./client/app/**/*.js', $.livereload.changed);
   gulp.watch('./client/**/*.html', $.livereload.changed);
 }
 
-function startInject(){
-  var target  = gulp.src( paths.index );
-  var scripts = gulp.src( paths.scripts, {read:false} );
-  var styles  = gulp.src( paths.styles, {read:false} );
 
-  return target
-    .pipe( $.inject( scripts,  {relative:true}) )
-    .pipe( $.inject( styles,  {relative:true}) )
-    .pipe( gulp.dest( paths.root ) );
-}
+gulp.task('bower', function () {
+  gulp.src( paths.index )
+      .pipe(wiredep())
+      .pipe(gulp.dest(paths.root));
+});
 
 })();
